cb197e4c8de104165424a565c47767a4dbd53b5e
Kajal More <2018mec004@sggs.ac.in>
2021-12-07 11:04:18 +0530

    Initial Commit

---
+++ opencv1.py
@@ -0,0 +1,19 @@
+
+import sys
+print("Python version")
+print(sys.version)
+
+#let's read and display an image
+
+import cv2
+img = cv2.imread('pic1.jpg')
+
+cv2.imshow('input', img)
+cv2.waitKey(1000)
+
+#Extractiong the Height and Width
+h,w = img.shape[:2]
+#print(h,w)
+print('Height = {}, Width = {} '.format(h,w))
+
+cv2.destroyAllWindows()
